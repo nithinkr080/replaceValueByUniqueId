3c2a3aa30ff05978a8a6b16d2887b571dc8de425
nithinkr080 <63775155+nithinkr080@users.noreply.github.com>
2024-03-28 16:23:23 +0530

    added replaceValueByUniqueId method

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as replaceValueByUniqueId } from './lib/replaceValueByUniqueId';
---
+++ src/lib/replaceValueByUniqueId.ts
@@ -0,0 +1,17 @@
+function replaceValueByUniqueId(
+  originalObject: any,
+  replacement: any,
+  identifierProperty: string = 'id',
+) {
+  if (typeof originalObject === 'object') {
+    for (const key in originalObject) {
+      if (originalObject[key][identifierProperty] == replacement[identifierProperty]) {
+        originalObject[key] = replacement;
+      }
+      replaceValueByUniqueId(originalObject[key], replacement, identifierProperty);
+    }
+  }
+  return originalObject;
+}
+
+export default replaceValueByUniqueId;
